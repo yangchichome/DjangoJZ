2c885c97251c200b6f5e909623c0b75d18442368
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-05-21 16:59:39 +0800

    backend_CRUD

--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'Index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+ 
