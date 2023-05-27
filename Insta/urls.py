b10600b9155a0a7d47c33d9678de039bd5605d9f
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-05-27 18:36:35 +0800

    Revert "update 0527"
    This reverts commit 1ad1c4e2ff8434de503247c2d23c26e33bbae809.

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld, PostsView, PostDetailView
+from Insta.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloWorld'),
     path('posts/', PostsView.as_view(), name='posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='make_post'),
+    path('posts/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+
 ]
