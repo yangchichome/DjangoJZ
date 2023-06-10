feef2f2a214d724edb939f73c2c7adb9b02966e3
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-06-10 10:01:00 +0800

    another

--- djangoP/settings.py
+++ djangoP/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'posts'
